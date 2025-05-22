ed3865c7b9953a9975dfff4d90709d15d6a87e2c
JVCarv41 <jvc.04.off@gmail.com>
2025-05-22 20:19:30 -0300

    authController now properly uses dotenv

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
